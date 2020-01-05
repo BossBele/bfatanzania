e16fa46b07d92c5fa8a614c09ca11aa30028a2ac
Adam Beleko <adambeleko@gmail.com>
2020-01-05 18:55:05 +0300

    added bfa site

---
+++ js/demo.js
@@ -0,0 +1,14 @@
+// import { CountUp } from 'https://cdn.jsdelivr.net/gh/inorganik/countUp.js/dist/countUp.js';
+
+// $(document).ready(function() {
+// 		setTimeout(function() {
+// 			var countUp1 = new CountUp('countup-members', 300);
+// 			var countUp2 = new CountUp('countup-members', 400);
+// 			var countUp3 = new CountUp('countup-members', 5);
+// 			var countUp4 = new CountUp('countup-members', 5);
+// 			countUp1.start();
+// 			countUp2.start();
+// 			countUp3.start();
+// 			countUp4.start();
+// 		}, 100);
+// });
